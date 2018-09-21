f6605d965011dd236019781c20a52c0f533bb057
benoit.charroux <benoit.charroux@EFREI89124M2-P.campus.villejuif>
2018-09-21 15:01:00 +0200

    rest client

--- src/main/java/com/example/CarRental/Car.java
+++ src/main/java/com/example/CarRental/Car.java
@@ -3,15 +3,17 @@ package com.example.CarRental;
 public class Car {
 	
 	private String plateNumber;
+	private String brand;
 	private int price;
 	
 	public Car() {
 		super();
 	}
 	
-	public Car(String plateNumber, int price) {
+	public Car(String plateNumber, String brand, int price) {
 		super();
 		this.plateNumber = plateNumber;
+		this.brand = brand;
 		this.price = price;
 	}
 	
@@ -23,6 +25,14 @@ public class Car {
 		this.plateNumber = plateNumber;
 	}
 	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
 	public int getPrice() {
 		return price;
 	}
--- src/main/java/com/example/CarRental/CarRentalService.java
+++ src/main/java/com/example/CarRental/CarRentalService.java
@@ -3,10 +3,12 @@ package com.example.CarRental;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 
@@ -16,14 +18,21 @@ public class CarRentalService {
 	private List<Car> cars = new ArrayList<Car>();
 	
 	public CarRentalService() {
-		cars.add(new Car("11AA22", 1000));
-		cars.add(new Car("33BB44", 2222));
+		cars.add(new Car("11AA22", "Ferrari", 1000));
+		cars.add(new Car("33BB44", "Porshe", 2222));
 	}
 	
 	@RequestMapping(value="/cars", method=RequestMethod.GET) 
 	@ResponseStatus(HttpStatus.OK) 
 	public List<Car> getListOfCars(){
 		return cars;
+	}
+	
+	@RequestMapping(value = "/cars", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void addCar(@RequestBody Car car) throws Exception{
+		System.out.println(car);
+		cars.add(car);
 	}
 
 }
